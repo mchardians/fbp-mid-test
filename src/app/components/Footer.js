e37a4a83bd7d4aa33806ea76e6b75ef73938ff56
rafioosadani <rafioosadani@gmail.com>
2025-04-25 07:20:06 +0700

    style: update padding footer layout

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -7,8 +7,8 @@ import FooterCopyright from "./FooterCopyright";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#165648] text-white">
-      <div className="container mx-auto px-4 py-14">
+    <footer className="bg-[#165648] text-white px-[100px] lg:px-[180px]">
+      <div className="container mx-auto py-14">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-16">
           <FooterLogo />
           <FooterLinks />
